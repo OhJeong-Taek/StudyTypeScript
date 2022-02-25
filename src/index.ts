0720843f077b0e6911c725e2299eaff92d67afc7
OhJeong-Taek <ojt0128@gmail.com>
2022-02-25 11:50:20 +0900

    remove distribution files

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const name = "JeongTaek",
 age = 31,
 gender = "male";
 
-const sayHi = (name, age, gender) => {
+const sayHi = (name: string, age: number, gender :string): void => {
     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`)
 };
 
